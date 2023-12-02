1d03bf8d402b829d9c3995e316a160aa131fb5c3
Mohit kumar <mohit.kumar@sendinblue.com>
2023-12-02 22:49:08 +0530

    Add chrome extension

---
+++ chrome_ext/service-worker.js
@@ -0,0 +1,57 @@
+
+// A generic onclick callback function.
+chrome.contextMenus.onClicked.addListener(genericOnClick);
+
+function genericOnClick(info) {
+
+  var newURL = chrome.runtime.getURL('FE/index.html');
+  
+  // var newURL = 'http://supportsage.dvl.to';
+  // var newURL = 'http://127.0.0.1:5500';
+  
+  let ss_query = '';
+  if (info.menuItemId == 'selection') {
+    ss_query = info.selectionText;
+  }
+  if (info.menuItemId == 'link') {
+    ss_query = info.linkUrl;
+  }
+  
+  if (ss_query != '') {
+    chrome.storage.sync.set({'ss_query': ss_query}, function() {
+      console.log('Settings saved');
+    });  
+  }
+  
+  chrome.tabs.create({ url: newURL });
+}
+
+chrome.runtime.onInstalled.addListener(function () {
+  let contexts = [
+    'page',
+    'link',
+    'selection'
+    // 'editable',
+    // 'image',
+    // 'video',
+    // 'audio'
+  ];
+  for (let i = 0; i < contexts.length; i++) {
+    let context = contexts[i];
+    let title = '';
+    if (context == 'page') {
+      title = "Go to SupportSage main page";
+    } else if (context == 'selection') {
+      title = "search selected text on SupportSage";
+    } else {
+      title = "search '" + context + "' on SupportSage";
+    }
+
+    chrome.contextMenus.create({
+      title: title,
+      contexts: [context],
+      id: context
+    });
+  }
+
+});
